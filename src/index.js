8b396422a9ad4f2fc8e72cb8a3b346ee08f1fa2d
TaohidHossain <htaohid31@gmail.com>
2024-10-14 11:48:20 +0600

    connect database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
-
+const { connectDB } = require('./Config')
+const { PORT } = require('./Config/serverConfig')
+// Setup express app
 const app = express()
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+// API routes
 
-app.listen(3000, ()=> {
-    console.log("app is listening at port 3000")
+// Connect DB and run the app
+app.listen(PORT, async ()=>{
+    await connectDB()
+    console.log("Database connected successfully")
+    console.log(`App is listening at port ${PORT}`)
 })
+
 
 app.get('/', (req, res) => {
     res.status(200).json({
