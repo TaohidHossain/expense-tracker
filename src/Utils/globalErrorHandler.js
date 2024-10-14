c18537b38662ee328291ffba71ea1e83a01422a2
TaohidHossain <htaohid31@gmail.com>
2024-10-14 22:45:00 +0600

    add return statment

--- src/Utils/globalErrorHandler.js
+++ src/Utils/globalErrorHandler.js
@@ -1,7 +1,7 @@
 module.exports = function(error, req, res, error){
     error.status = error.status || 'error'
     error.statusCode = error.statusCode || 500
-    res.status(error.statusCode).json({
+    return res.status(error.statusCode).json({
         "status": error.statusCode < 500 ? 'fail' : 'error',
         "statusCode": error.statusCode,
         "message": error.message,
