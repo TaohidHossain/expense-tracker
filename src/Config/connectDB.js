7216efe38201be8d900bbc85130f84f1eb29cb60
TaohidHossain <htaohid31@gmail.com>
2024-10-14 11:05:33 +0600

    add configuration files

---
+++ src/Config/connectDB.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+const host = process.env.DB_HOST
+const username = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
+const collection = process.env.DB_COLLECTION_NAME
+const cluster = process.env.DB_CLUSTER
+
+const DB_URL = host + "://" + username + ':' + password + "@" + cluster + '/' + collection + "?retryWrites=true&w=majority&appName=Cluster0"
+
+async function connectDB(){
+    try {
+        await mongoose.connect(DB_URL)
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports = connectDB
