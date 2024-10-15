635ca078aeeb92f26c4f7c3c59ae54067b1034dd
TaohidHossain <htaohid31@gmail.com>
2024-10-15 14:55:36 +0600

    add getExpenses method

--- src/Controllers/expenseController.js
+++ src/Controllers/expenseController.js
@@ -1,8 +1,29 @@
+const { query } = require('express')
 const { Expense } = require('../Models')
 const { asyncErrorHandler } = require('../Utils')
 
 const getExpenses = asyncErrorHandler( async (req, res, next) => {
+    let queryObj = { ...req.query }
+    queryObj['userId'] = req.user._id
+    // Pagination logic
+    let page = req.query.page ? req.query.page : 1 // By default show first page
+    let limit = req.query.limit ? req.query.limit : 10 // By default limit to 10 expenses
+    let skip = (page - 1) * limit
+    // Sorting and Selecting fields logic
+    let sort = req.query.sort ? req.query.sort.split(',').join(' ') :  '-createdAt'
+    let fields = req.query.fields ? req.query.fields.split(',').join(' ') : '-__v'
     
+    delete queryObj['page']
+    delete queryObj['limit']
+    delete queryObj['sort']
+    delete queryObj['fields']
+
+    let query = Expense.find(queryObj)
+    const expenses = await query.sort(sort).skip(skip).limit(limit).select(fields)
+    return res.status(200).json({
+        "status": "success",
+        "data": expenses
+    })
 })
 
 const getExpense = asyncErrorHandler( async (req, res, next) => {
